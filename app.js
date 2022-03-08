fa2963615eb9c2aa0e3bcac07c2f326b05b3372b
Jeff Seaman <jseaman@pointpark.edu>
2022-03-08 00:26:36 -0500

    Added for test only

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require("express-session");
 const flash = require('express-flash');
 const favicon = require('serve-favicon');
 
-const passport = require('./src/config/passport.config');
+//const passport = require('./src/config/passport.config');
 const configureI18N = require('./src/config/i18n.config');
 
 const i18n = configureI18N(__dirname);
